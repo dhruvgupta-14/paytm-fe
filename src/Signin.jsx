32470c372912541e8d312d58c987ff544377aef8
dhruvgupta-14 <dhruvpahariya692@gmail.com>
2025-06-05 08:42:54 +0530

    change in sign in

--- src/Signin.jsx
+++ src/Signin.jsx
@@ -7,29 +7,36 @@ import { InputBox } from "./component/InputBox";
 import { Button } from "./component/Button";
 import { BottomWarning } from "./component/BottomWarning";
 import toast from "react-hot-toast";
-const apiUrl=import.meta.env.VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_URL;
 export const Signin = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-    useEffect(()=>{
-   const token=localStorage.getItem("token")
-   if(token) navigate('/dashboard')
-  },[])
-    const submitHandler=async()=>{
-    try{
-      const response =await axios.post(apiUrl+"/signin",{
-        username,password
-      })
-      const token=response.data.token
-      localStorage.setItem("token",token)
-      toast.success('Signin Successfully')
-      navigate('/dashboard')
-    }catch(e){
-      toast.error('Internal Server error,Try again after Some Time')
-      console.log(e)
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) navigate("/dashboard");
+  }, []);
+  const submitHandler = async () => {
+    try {
+      const response = await axios.post(apiUrl + "/signin", {
+        username,
+        password,
+      });
+      console.log(response)
+      if (response.data.success) {
+        const token = response.data.token;
+        localStorage.setItem("token", token);
+        toast.success("Signin Successfully");
+        navigate("/dashboard");
+      }
+    } catch (e) {
+      toast.error(
+        e.response.data.message ||
+          "Internal server error try again after some time"
+      );
+      console.log("sign in",e);
     }
-  }
+  };
 
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
@@ -52,10 +59,7 @@ export const Signin = () => {
             label={"Password"}
           />
           <div className="pt-4">
-            <Button
-              onClick={submitHandler}
-              label={"Sign in"}
-            />
+            <Button onClick={submitHandler} label={"Sign in"} />
           </div>
           <BottomWarning
             label={"Don't have an account?"}
